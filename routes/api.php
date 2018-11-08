d58844dc685d5a6d798fba9092fa335e95f13229
Jace Ju <jaceju@kkbox.com>
2018-11-08 18:18:24 +0800

    Step 5 - Implement API

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// 註：這邊只是為了示範，所以沒有特別將程式碼優化或考慮任何安全性。
+Route::middleware('api')->post('/users', function (Request $request) {
+    \App\User::create($request->all());
+    return new \Illuminate\Http\JsonResponse([], 201);
 });
