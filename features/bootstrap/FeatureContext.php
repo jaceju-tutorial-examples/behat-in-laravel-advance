5f69fd54830ce2fc716af88bb110d8ff699a6866
Jace Ju <jaceju@kkbox.com>
2018-11-08 15:42:05 +0800

    Step 4 - Implement step definitions

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,7 +1,6 @@
 <?php
 
 use Behat\Behat\Context\Context;
-use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\TableNode;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -32,42 +31,63 @@ class FeatureContext extends TestCase implements Context
     }
 
     /**
-     * @Given API 網址為 :arg1
+     * @var string
      */
-    public function apiWangZhiWei($arg1)
+    private $apiUrl = '';
+
+    /**
+     * @var array
+     */
+    private $apiBody = [];
+
+    /**
+     * @var \Illuminate\Foundation\Testing\TestResponse
+     */
+    private $response;
+
+    /**
+     * @Given API 網址為 :apiUrl
+     * @param string $apiUrl
+     */
+    public function apiUrl(string $apiUrl)
     {
-        throw new PendingException();
+        $this->apiUrl = $apiUrl;
     }
 
     /**
      * @Given API 附帶資料為
+     * @param TableNode $table
      */
-    public function apiFuDaiZiLiaoWei(TableNode $table)
+    public function apiBody(TableNode $table)
     {
-        throw new PendingException();
+        $this->apiBody = $table->getHash()[0];
     }
 
     /**
-     * @When 以 :arg1 方法要求 API
+     * @When 以 :method 方法要求 API
+     * @param string $method
      */
-    public function yiFangFaYaoQiuApi($arg1)
+    public function request(string $method)
     {
-        throw new PendingException();
+        $this->response = $this->json($method, $this->apiUrl, $this->apiBody);
     }
 
     /**
-     * @Then 回傳狀態應為 :arg1
+     * @Then 回傳狀態應為 :statusCode
+     * @param int $statusCode
      */
-    public function huiChuanZhuangTaiYingWei($arg1)
+    public function assertStatus(int $statusCode)
     {
-        throw new PendingException();
+        $this->response->assertStatus($statusCode);
     }
 
     /**
-     * @Then 資料表 :arg1 應有資料
+     * @Then 資料表 :tableName 應有資料
+     * @param string $tableName
+     * @param TableNode $table
      */
-    public function ziLiaoBiaoYingYouZiLiao($arg1, TableNode $table)
+    public function assertTableRecordExisted(string $tableName, TableNode $table)
     {
-        throw new PendingException();
+        $this->assertDatabaseHas($tableName, $table->getHash()[0]);
     }
 }
